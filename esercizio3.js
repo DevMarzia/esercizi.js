a723d3952b4255c185815eb6e24fff200ecb4991
unknown <devmars.mb@gmail.com>
2023-06-10 21:25:44 +0200

    3 esercizio

---
+++ esercizio3.js
@@ -0,0 +1,19 @@
+/*Esercizio 3
+
+- Crea 3 variabili: `area`, `x` e `y`.
+- Se `x` e `y` rappresentano i lati di un rettangolo, assegna loro due valori numerici e usa la variabile `area` per calcolare l'area del rettangolo.
+- Stampa in console il valore dell'area*/
+
+
+let rettangolo={
+    base:5,
+    altezza:6
+};
+
+let areaRettangolo = rettangolo.base * rettangolo.altezza;
+
+/*la base e l'altezza si trovano all'interno della variabile rettangolo,
+ per accedere al loro valore bisogna richiamare il nome della variabile eseguito dal punto*/
+
+console.log(areaRettangolo);
+
