398d84b3fc2b135b62229aef91f377879f694c38
unknown <devmars.mb@gmail.com>
2023-06-10 21:15:44 +0200

    2 esercizio

---
+++ esercizio2.js
@@ -0,0 +1,14 @@
+/*let name = 'mario';
+let *surname = 'Rossi';
+let 1age = 20;
+let ?isMarried = false;
+let job? = 'developer';
+let isGraduated2 = false;*/
+
+
+let name='Mario';
+let surname='Rossi';
+let age=20;
+let isMarried=false;
+let job='developer';
+let isGraduated2=false;
